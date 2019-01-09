832833e8b190065897e62913b96a7583847d58e4
adrioro <artistsvillage@me.com>
2019-01-09 16:13:24 -0500

    second commit 1-Create a new html file. 2-Code 2 different elements, with 2 different IDs and onclick attributes. 3-Create a function to change the styling of one of the elements when clicked.

---
+++ index.js
@@ -0,0 +1,11 @@
+function changeColor() {
+  document.getElementById("h1Id").style.color = "green";
+}
+
+function divOneFunction() {
+  alert("I clicked the text and got this alert! It works!!!");
+}
+
+function changeText() {
+  document.getElementById("divOneId").innerHTML = "<div><b>I replaced my old Div. This is my new Div! It has a new text and it is bold.<b></div>";
+}
